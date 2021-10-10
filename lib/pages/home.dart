b176435a573af55f63b487a2462519d0924e8445
Nebstar Malala <56993122+nebstarmalala@users.noreply.github.com>
2021-10-10 13:49:44 +0300

    set background color and image using ternary operator

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,41 +12,64 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
+    // Receive time data
     data = ModalRoute.of(context).settings.arguments;
-    print(data);
+
+    //set background Image
+    String bgImage = data['isDaytime'] ? 'day.jpg' : 'night.jpg';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
 
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                  onPressed: () {Navigator.pushNamed(context, '/location');},
-                  icon: Icon(Icons.edit_location),
-                  label: Text("Choose Location")
-              ),
-              SizedBox(height: 20.0,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    data['location'],
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {Navigator.pushNamed(context, '/location');},
+                    icon: Icon(
+                        Icons.edit_location,
+                        color: Colors.grey[200],
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
+                    label: Text(
+                        "Choose Location",
+                        style: TextStyle(
+                          color: Colors.grey[200]
+                        )
+                    )
                 ),
-              ),
-            ],
+                SizedBox(height: 20.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
